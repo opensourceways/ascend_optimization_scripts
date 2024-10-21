51891c6fee583cfcf86527b584b0edeeccb83841
姚小白 <17625331900@163.com>
2024-10-21 11:06:09 +0800

    opensourceway script

--- scripts/config.py
+++ scripts/config.py
@@ -5,9 +5,6 @@ table_header = """
     <tr>
         <th>检查项</th>
         <th>状态</th>
-        <th colspan="2">详情</th>
-        <th>日志</th>
-        <th>出包</th>
     </tr>
 """
 
@@ -15,32 +12,18 @@ table_body = """
 <tr>
     <td>{0}</td>
     <td>&#{1};</td>
-    <td colspan="2">{2}</td>
-    <td>{3}</td>
-    <td>{4}</td>
 </tr>
 """
 
 # 检查项映射表, key为标准命名
 check_name_map = {
-    "sca": ["sca", "codecheck_scan", "CodeSCA", "CodeSCA检查"],
-    "anti_poison": ["anti_poison", "codecheck_ftd", "防投毒扫描"],
-    "code_check": ["code_check", "codecheck", "codecheck扫描", "CodeCheck代码检查"],
-    "dt_check": ["dt_check", "DT"],
-    "build": ["build", "Build构建", "pr构建编译", "build构建"],
-    "build_arm": ["build_arm", "Build_ARM"],
-    "build_x86": ["build_x86", "Build_X86"],
+    "sca": ["sca"],
+    "anti_poison": ["anti_poison", "防投毒扫描"],
+    "build": ["build", "Build构建", "pr构建编译"],
 }
 
-GiteeAddr = "https://gitee.com/api/v5/repos/"  # Gitee 接口地址
+GithubAddr = "https://api.github.com/repos"  # Gitee 接口地址
 
 # 北京四区
-CodeBuildAddr = "https://cloudbuild-ext.cn-north-4.myhuaweicloud.com"  # 编译地址
 PipelineAddr = "https://cloudpipeline-ext.cn-north-4.myhuaweicloud.com"  # 流水线地址
-CodeArtsAddr = "https://devcloud.cn-north-4.huaweicloud.com"
-# HWCloudAddr = "https://auth.huaweicloud.com/authui/federation/websso?domain_id=0d336831568090260fd5c01450729240&idp=OneAcess&protocol=saml"
 HWIAMAddr = "https://iam.cn-north-4.myhuaweicloud.com/v3/auth/tokens"
-
-OBSName = "mindstudio-pr-log"
-
-NA = "N/A"
---
+++ scripts/monitor.py
@@ -0,0 +1,166 @@
+#! -*- coding: utf-8 -*-
+
+import argparse
+import json
+import requests
+import logging
+
+from config import GithubAddr, check_name_map, PipelineAddr, table_header, table_body, HWIAMAddr
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s  %(levelname)s: %(message)s")
+
+status_map = dict(COMPLETED="9989", RUNNING="128346", CANCELED="10060", FAILED="10060")
+
+
+class GithubApp:
+
+    def __init__(self,
+                 token: str,
+                 owner: str,
+                 repo: str,
+                 pr_id: str
+                 ):
+        """
+        @token: github token
+        @owner: 代码仓所属企业
+        @repo: 代码仓名称
+        @pr_id: 提交pr id
+        """
+        self.token = token
+        self.owner = owner
+        self.repo = repo
+        self.pr_id = pr_id
+        self.msg_remark_url = f'{GithubAddr}/{owner}/{repo}/pulls/{pr_id}/comments'
+
+    def add_comment(self, msg: str):
+        """
+        @msg: 评论内容
+        """
+        logging.info(f"comment url: {self.msg_remark_url}")
+        response = requests.post(self.msg_remark_url, data=dict(access_token=self.token, body=msg))
+        if response.status_code in [200, 201, 204]:
+            logging.info(f'comment success')
+        else:
+            logging.error(f'comment failed')
+
+
+class CheckListRemark:
+
+    def __init__(self,
+                 token: str,
+                 owner: str,
+                 repo: str,
+                 pr_id: str,
+                 project_id: str,
+                 pipeline_id: str,
+                 pipeline_run_id: str,
+                 username: str,
+                 subUsername: str,
+                 password: str,
+                 ):
+        """
+        @token: github token
+        @owner: 代码仓所属企业
+        @repo: 代码仓名称
+        @pr_id: 待合入pr id
+        @project_id: codearts 项目id
+        @pipeline_id: codearts 流水线id
+        @pipeline_run_id: codearts 流水线任务id
+        """
+        self.token = token
+        self.owner = owner
+        self.repo = repo
+        self.pr_id = pr_id
+        self.project_id = project_id
+        self.pipeline_id = pipeline_id
+        self.pipeline_run_id = pipeline_run_id
+        self.username = username,
+        self.subUsername = subUsername,
+        self.password = password,
+        self.git_app = GithubApp(token, owner, repo, pr_id)
+
+    @staticmethod
+    def convert_check_name_map():
+        """
+        将检查项映射表key, value翻转过来
+        """
+        res = {}
+        for k, v in check_name_map.items():
+            for i in v:
+                res[i] = k
+        return res
+
+    @staticmethod
+    def generate_table(items: list):
+        """
+        将检查项结果转换成html table
+        """
+        html = table_header
+        for item in items:
+            check_name, status = item.get("check_name"), item.get("status")
+            html += table_body.format(check_name, status)
+        html = html + "</table>"
+        return html
+
+    def get_codearts_token(self):
+        logging.info("获取codearts token...")
+        user = dict(password=self.password, domain=self.username, name=self.subUsername)
+        header = {
+            "auth": {
+                "identity": {
+                    "password": {"user": user},
+                    "methods": ["password"]
+                },
+                "scope": {
+                    "project": {"name": "cn-north-4"}
+                }
+            }
+        }
+        req_get_token = requests.post(url=HWIAMAddr, data=json.dumps(header))
+        token = req_get_token.headers["X-Subject-Token"]
+        return {"x-auth-token": token}
+
+    def run(self):
+        # job_name_map = self.convert_check_name_map()
+        result = []
+        task_url = f'{PipelineAddr}/v5/{self.project_id}/api/pipelines/{self.pipeline_id}/pipeline-runs/detail?pipeline_run_id={self.pipeline_run_id}'
+        resp = requests.get(url=task_url, headers=self.get_codearts_token())
+        resp_txt = json.loads(resp.text)
+        for stage in resp_txt["stages"]:
+            for j in stage["jobs"]:
+                job_name, status = j["name"], j["status"]
+                result.append(dict(check_name=job_name, status=status_map.get(status)))
+
+        html = self.generate_table(result)
+        self.git_app.add_comment(html)
+
+
+def init_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--access_token', help='gitub access token', required=True, type=str)
+    parser.add_argument('--owner', help='owner', required=True, type=str)
+    parser.add_argument('--pr_id', help='pr id', required=True, type=str)
+    parser.add_argument('--repo', help='code repo', required=True, type=str)
+    parser.add_argument('--project_id', help='current pipeline project id', type=str, default=None, required=False)
+    parser.add_argument('--pipeline_id', help='current pipeline id', type=str, default=None, required=False)
+    parser.add_argument('--pipeline_run_id', help='current pipeline run id', type=str, default=None, required=False)
+    parser.add_argument('--username', help='codearts username', required=True, type=str)
+    parser.add_argument('--subUsername', help='codearts subUsername', required=True, type=str)
+    parser.add_argument('--password', help='codearts password', required=True, type=str)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = init_args()
+    checklist_remark = CheckListRemark(token=args.access_token,
+                                       owner=args.owner,
+                                       repo=args.pr_id,
+                                       pr_id=args.repo,
+                                       project_id=args.project_id,
+                                       pipeline_id=args.pipeline_id,
+                                       pipeline_run_id=args.pipeline_run_id,
+                                       username=args.username,
+                                       subUsername=args.subUsername,
+                                       password=args.password,
+                                       )
+    checklist_remark.run()
