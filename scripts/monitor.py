9bbf111c61d4f4e3d37005a1c7115bbed609284b
Hourunze1997 <1043170898@qq.com>
2024-11-30 12:03:42 +0800

    add mails

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -98,7 +98,7 @@ class GithubApp:
     @retry_request
     def send_mail(self, msg: str, host: str, port: str, username: str, password: str,
                   sender: str, mails: list, owner: str, repo: str):
-        subject = "%s服务定时检查结果通知" % repo
+        subject = "%s门禁检查结果通知" % repo
         mails.extend(["shishupei@huawei.com", "zhaokunhang@huawei.com", "jun.zhongjun@huawei.com"])
 
         mails = list(set(mails))
